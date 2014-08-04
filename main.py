b30036b1ff7f33bd0ae668fc887f31a10d4f1d7e
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2014-08-03 23:41:36 -0300

    Alteração da estrutura do código, modificado para uma classe. A alteração visa introduzir a abstração de objetos.

--- main.py
+++ main.py
@@ -4,44 +4,60 @@ import tkinter
 
 from tkinter import *
 
-app = tkinter.Tk()
+class Desenho:
 
-#cria a area canvas
-canvas = tkinter.Canvas(app, bg="gray", height=400, width = 600)
-canvas.pack( side = TOP )
+    app    = None
+    frame  = None
+    canvas = None
 
-#cria os frames para organizar o conteudo
-frame = Frame(app)
-frame.pack()
+    def __init__(self):
 
-#define as funcoes para manipulacao do canvas
-def bandeira():
-    limpar()
-    # parametros x1, y1, x2, y2
-    canvas.create_rectangle(100,50,500,350, outline="",fill="green")
-    # parametros xn, yn por ponto
-    canvas.create_polygon([300, 100, 450, 200, 300, 300, 150, 200], outline="",fill="yellow")
-    # parametros x1, y1, x2, y2
-    canvas.create_oval(250, 150, 350, 250, outline="", fill="blue")
-def estrela():
-    limpar()
-    # parametros xn, yn por ponto
-    #canvas.create_polygon([300,50, 350,150, 500,150, 390,240, 430,350, 300,290, 170,350, 210,240, 100,150, 250,150], outline="black",fill="",width=2)
-    canvas.create_polygon([300,50, 440,350, 100,150, 500,150, 160,350], outline="",fill="white")
-def limpar():
-    canvas.delete("all")
+        # Inicia o tkinter
+        self.app = tkinter.Tk()
 
-# Componentes do painel
-labelMenu = Label(frame, text="Menu", fg="black", height=2, width=10)
-labelMenu.pack( side = LEFT)
+        # Cria a area canvas
+        self.canvas = tkinter.Canvas(self.app, bg="gray", height=400, width = 600)
+        self.canvas.pack( side = TOP )
 
-btnLimpar = Button(frame, text="Limpar", fg="red", height=2, width=10, command=limpar)
-btnLimpar.pack( side = LEFT)
+        # Cria os frames para organizar o conteudo
+        self.frame = Frame(self.app)
+        self.frame.pack()
 
-btnBandeira = Button(frame, text="Bandeira", fg="blue", height=2, width=10, command=bandeira)
-btnBandeira.pack( side = RIGHT )
+        # Adiciona os elementos
+        self.componentes()
 
-btnEstrela = Button(frame, text="Estrela", fg="blue", height = 2, width = 10, command=estrela)
-btnEstrela.pack( side = RIGHT )
+        # "Executável do programa"
+        self.app.mainloop()
 
-app.mainloop()
+    # Define as funcoes para manipulacao do canvas
+    def bandeira(self):
+        self.limpar()
+        # Parametros x1, y1, x2, y2
+        self.canvas.create_rectangle(100,50,500,350, outline="",fill="green")
+        # Parametros xn, yn por ponto
+        self.canvas.create_polygon([300, 100, 450, 200, 300, 300, 150, 200], outline="",fill="yellow")
+        # Parametros x1, y1, x2, y2
+        self.canvas.create_oval(250, 150, 350, 250, outline="", fill="blue")
+    def estrela(self):
+        self.limpar()
+        # Parametros xn, yn por ponto
+        # Canvas.create_polygon([300,50, 350,150, 500,150, 390,240, 430,350, 300,290, 170,350, 210,240, 100,150, 250,150], outline="black",fill="",width=2)
+        self.canvas.create_polygon([300,50, 440,350, 100,150, 500,150, 160,350], outline="",fill="white")
+    def limpar(self):
+        self.canvas.delete("all")
+
+    # Componentes do painel
+    def componentes(self):
+        labelMenu = Label(self.frame, text="Menu", fg="black", height=2, width=10)
+        labelMenu.pack( side = LEFT)
+
+        btnLimpar = Button(self.frame, text="Limpar", fg="red", height=2, width=10, command=self.limpar)
+        btnLimpar.pack( side = LEFT)
+
+        btnBandeira = Button(self.frame, text="Bandeira", fg="blue", height=2, width=10, command=self.bandeira)
+        btnBandeira.pack( side = RIGHT )
+
+        btnEstrela = Button(self.frame, text="Estrela", fg="blue", height = 2, width = 10, command=self.estrela)
+        btnEstrela.pack( side = RIGHT )
+
+Desenho()
